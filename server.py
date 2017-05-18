d17b67499f8c06ae5e14f7f0f11940e910b9cf41
Drew Pham <drewvpham@gmail.com>
2017-05-18 14:14:19 -0500

    more worked done

--- server.py
+++ server.py
@@ -14,16 +14,29 @@ def index():
 
     return render_template('index.html', emails=emails)
 
-@app.route('/create', methods=["POST"])
-def create():
-    query = "insert into emails (email, date) values (:email, now())"
+@app.route('/register', methods=["POST"])
+def register():
 
-    data = {
-        'email': request.form["email"],
-    }
+    errors={}
+    if len(request.form['first_name']) > 2 or not request.form['first_name'].isalpha():
+        errors['first_name']='First Name must be at least 3 characters in length and all alphabetic characters.'
+        #flash("Email cannot be blank!")
+    if len(request.form['last_name']) > 2 or not request.form['last_name'].isalpha():
+        errors['last_name']='Last Name must be at least 3 characters in length and all alphabetic characters.'
+    if len(request.form['email']) is 0:
+        errors['email']'Email can not be empty'
+    if not EMAIL_REGEX.MATCH(request.form['email']):
+        errors['email']='Invalid email format.'
+    if len(request.form['password']) < 8:
+        errors['password']='Password must be at least 8 characters in length'
+    if request.form['password'] is not request.form['password_confirmation']:
+        errors['password']='Password and Password Confirmation do not match'
 
-    if len(request.form['email']) < 1:
-        flash("Email cannot be blank!")
+    if errors:
+        flash(errors)
+
+
+
     elif not EMAIL_REGEX.match(request.form['email']):
         flash("Invalid Email Address!")
     else:
